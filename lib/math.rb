dbe5c40c5055a5b27dcdaeb0bc42481b8766a58d
michael onyeije <monyeije16@saintpeters.edu>
2020-09-24 16:18:59 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,39 @@
 def addition(num1, num2)
+  arr = num2 + num1
+  puts arr
+  return arr
 
 end
 
 def subtraction(num1, num2)
+  arr =  num1 - num2 
+  puts arr
+  return arr
 
 end
 
 def division(num1, num2)
+  arr = num1 / num2 
+  puts arr
+  return arr
 
 end
 
 def multiplication(num1, num2)
+  arr = num2 * num1
+  puts arr
+  return arr
 
 end
 
 def modulo(num1, num2)
-
+   arr = num1 % num2
+  puts arr
+  return arr
 end
 
 def square_root(num)
-
+   arr = Integer.sqrt(num)
+  puts arr
+  return arr
 end
